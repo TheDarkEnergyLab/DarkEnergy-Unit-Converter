16cbd1306c8f8be858fa6a8ae4c5c1d576efe8cf
MartianDrummer <marvinhill@mac.com>
2024-04-08 21:49:54 -0400

    Initial Commit

---
+++ DarkEnergy Unit Converter/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  DarkEnergy Unit Converter
+//
+//  Created by Marvin on 4/8/24.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
---
+++ DarkEnergy Unit ConverterUITests/DarkEnergy_Unit_ConverterUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  DarkEnergy_Unit_ConverterUITestsLaunchTests.swift
+//  DarkEnergy Unit ConverterUITests
+//
+//  Created by Marvin on 4/8/24.
+//
+
+import XCTest
+
+final class DarkEnergy_Unit_ConverterUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
